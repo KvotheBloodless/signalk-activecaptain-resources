8f50b59af1af1dfc50af1b5a70fcf9476bfd5481
Paul Willems <paul.willems@gmail.com>
2024-11-19 11:18:54 +0100

    Added lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,29 @@
-{
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "rules": {
-        "semi": 2
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  }, 
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended"
+  ],
+  plugins: [
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+  ],
+  rules: {
+    strict: 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    react: {
+      version: "detect"
     }
+  }
 }
